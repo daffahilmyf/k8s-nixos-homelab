77792fdb7c5dccf2422f7d071e581ab76d7fd506
Daffa Hilmy Fadhlurrohman <daffahilmanafrizal@gmail.com>
2025-11-16 15:59:47 +0700

    [ADD]: flake skeleton

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  description = "Lightweight industrial k3s NixOS cluster";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    sops-nix.url = "github:Mic92/sops-nix";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    sops-nix,
+  } @ inputs: let
+    sharedModules = [
+      ./modules/base.nix
+      ./modules/networking.nix
+      ./modules/packages.nix
+      ./modules/k3s.nix
+      ./modules/node-role.nix
+      ./modules/cluster-bootstrap.nix
+      ./modules/sops.nix
+      ./modules/cloudflared.nix
+      ./modules/git.nix
+      ./modules/ssh.nix
+    ];
+  in {
+    nixosConfigurations = {
+      k3s-control-1 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        # Pass flake inputs to modules (required for sops)
+        specialArgs = {inherit inputs;};
+
+        modules =
+          sharedModules
+          ++ [
+            ./hosts/k3s-control-1.nix
+          ];
+      };
+
+      k3s-worker-1 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        specialArgs = {inherit inputs;};
+
+        modules =
+          sharedModules
+          ++ [
+            ./hosts/k3s-worker-1.nix
+          ];
+      };
+
+      k3s-worker-2 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        specialArgs = {inherit inputs;};
+
+        modules =
+          sharedModules
+          ++ [
+            ./hosts/k3s-worker-2.nix
+          ];
+      };
+    };
+  };
+}
