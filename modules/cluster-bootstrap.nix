24bca584391a42df13f44ca0d49f30938201e6b3
Daffa Hilmy Fadhlurrohman <daffahilmanafrizal@gmail.com>
2025-11-19 21:31:53 +0700

    [ADD] disabled servicelb

--- modules/cluster-bootstrap.nix
+++ modules/cluster-bootstrap.nix
@@ -3,13 +3,15 @@
   config,
   ...
 }:
+
 let
   isControl = config.custom.role == "control-plane";
   isWorker = config.custom.role == "worker";
   tokenPath = config.sops.secrets.k3s_token.path;
+
   sopsServiceName = "sops-nix";
   hasSopsService = builtins.hasAttr sopsServiceName config.systemd.services;
-  sopsDeps = lib.optionals hasSopsService ["${sopsServiceName}.service"];
+  sopsDeps = lib.optionals hasSopsService [ "${sopsServiceName}.service" ];
 in {
   options.custom.cluster.apiServer = lib.mkOption {
     type = lib.types.str;
@@ -31,18 +33,22 @@ in {
 
     services.k3s.tokenFile = tokenPath;
 
-    services.k3s.extraFlags = lib.concatStringsSep "\n" (
-      lib.optional isControl "--disable traefik"
-      ++ lib.optional isControl "--token-file ${tokenPath}"
-      ++ lib.optional isWorker ''
-        --server ${config.custom.cluster.apiServer}
-        --token-file ${tokenPath}
-      ''
-    );
+    services.k3s.extraFlags =
+      lib.optionals isControl [
+        "--disable traefik"
+        "--disable servicelb"
+      ]
+      ++ lib.optionals isControl [
+        "--token-file ${tokenPath}"
+      ]
+      ++ lib.optionals isWorker [
+        "--server ${config.custom.cluster.apiServer}"
+        "--token-file ${tokenPath}"
+      ];
 
     systemd.services.k3s = {
-      wants = ["network-online.target"] ++ sopsDeps;
-      after = ["network-online.target"] ++ sopsDeps;
+      wants = [ "network-online.target" ] ++ sopsDeps;
+      after = [ "network-online.target" ] ++ sopsDeps;
       requires = sopsDeps;
     };
   };
