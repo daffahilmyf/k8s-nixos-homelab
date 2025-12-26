fec01bec8792a95ec241a9e95d24af4fbafaff14
Daffa Hilmy Fadhlurrohman <daffahilmanafrizal@gmail.com>
2025-12-27 00:01:53 +0700

    [OPS] victoria

--- modules/cluster-hosts.nix
+++ modules/cluster-hosts.nix
@@ -1,15 +1,15 @@
 {...}: {
   # Ensure every host in the flake can resolve the cluster nodes locally.
   networking.hosts = {
-    "192.168.100.160" = [
+    "192.168.100.150" = [
       "k3s-control-1"
       "k3s-control-1.home.arpa"
     ];
-    "192.168.100.161" = [
+    "192.168.100.151" = [
       "k3s-worker-1"
       "k3s-worker-1.home.arpa"
     ];
-    "192.168.100.162" = [
+    "192.168.100.152" = [
       "k3s-worker-2"
       "k3s-worker-2.home.arpa"
     ];
